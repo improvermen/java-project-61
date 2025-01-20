aa77adec507593a7e62ab150f3aa18fcfde3f734
improvermen <116031130+improvermen@users.noreply.github.com>
2025-01-21 00:26:12 +0500

    step 3 test

--- src/main/java/hexlet/code/Main.java
+++ src/main/java/hexlet/code/Main.java
@@ -1,7 +1,31 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Cli.greetUser();
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            System.out.println("Please enter the game number and press Enter.");
+            System.out.println("1 - Greet");
+
+            System.out.println("0 - Exit");
+            System.out.println("Enter your choice: ");
+
+            int choice = sc.nextInt();
+            switch (choice) {
+
+                case 1:
+                    Cli.greetUser();
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Exiting the game. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        sc.close();
     }
 }
